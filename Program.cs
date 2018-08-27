7f0a7e5d3213fd706fd076b4d7acf518d831a544
leahgwin <leahgwin@gmail.com>
2018-08-27 13:43:18 -0500

    made classes for exercise and student

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace lightningExercises
+{
+    public class Exercise
+    {
+
+        public string Name { get; set; }
+        public string GithubUrl { get; set; }
+        public string Language { get; set; }
+
+    }
+
+
+}
---
+++ Student.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace lightningExercises
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        public string fullName = { $"{Student.firstName} {Student.lastName}" }
+
+        public string name(string, string)
+        {
+
+        }
+
+        public List<Exercise> exercisesList = new List<Exercise>();
+
+
+
+    }
+}
